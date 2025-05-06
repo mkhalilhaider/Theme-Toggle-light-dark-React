cc4bd7591aca60ccbcdec84aba29ef810566fba2
mkhalilhaider <khalil.haider2109@gmail.com>
2025-05-06 17:30:59 +0500

    Used ThemeContext in App.jsx and added dark/light mode logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { ThemeProvider } from "./contexts/Theme";
+import ThemeBtn from "./components/ThemeBtn";
+import Card from "./components/Card";
+import CreatedBy from "./components/CreatedBy";
 
 function App() {
+  const [themeMode, setThemeMode] = useState("light");
+
+  const darkMode = () => {
+    setThemeMode("dark");
+  };
+  const lightMode = () => {
+    setThemeMode("light");
+  };
+
+  useEffect(() => {
+    const theme = document.querySelector("html");
+    theme.classList.remove("light", "dark");
+    theme.classList.add(themeMode);
+  }, [themeMode]);
+
   return (
-    <>
-    <h1 className="bg-green-700">hisadhsa</h1>
-      <div></div>
-      <div></div>
-    </>
+    <ThemeProvider value={{ themeMode, darkMode, lightMode }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeBtn />
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
   );
 }
 
