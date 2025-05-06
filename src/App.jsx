a3d5f86646b4f479e6f54eecff6ebcba2bd94c79
mkhalilhaider <khalil.haider2109@gmail.com>
2025-05-06 15:57:06 +0500

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+    <h1 className="bg-green-700">hisadhsa</h1>
+      <div></div>
+      <div></div>
+    </>
+  );
+}
+
+export default App;
